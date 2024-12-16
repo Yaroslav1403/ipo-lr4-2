9f83cdaacd95d43bf85cc6a9a034562d9758704a
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-12-16 20:31:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ float_numbers = [3.14, 99.71, 5.71, -55.33, 2.5]
 #Преобразование в целые числа
 int_numbers = [int(num) for num in float_numbers]
 #Вывод содержания списка
-print(int_numbers)
+print(int_numbers) 
